e005145b0fcd10fd5e14ad5da4568a47a36bd811
Yibo <yiboliang02@gmail.com>
2023-12-05 11:34:25 -0800

    shiny

--- shiny.R
+++ shiny.R
@@ -3,6 +3,21 @@
 
 library(shiny)
 
+us_species <- read_csv("data/dat_species.csv")
+us_species <- us_species %>% filter(sciname == "Myocastor coypus" | sciname == "Hydrochoerus hydrochaeris")
+us_geo <- read_csv("data/dat_spatial.csv")
+us_geo <- us_geo %>% 
+  filter(name != "Hawaii" & name != "Alaska")
+
+colnames(us_geo)[1] <- "state"
+us_geo_merged <- merge(us_geo,us_species,by="state", all=T)
+names(us_geo_merged) 
+
+us_geo_merged <- us_geo_merged[is.na(us_geo_merged$geometry) == FALSE, ]
+
+# Create an sf object (SQ) from the WKT geometries
+SQ <- st_as_sf(us_geo_merged, wkt = "geometry")
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
@@ -12,16 +27,9 @@ ui <- fluidPage(
     # Sidebar with a slider input for 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("years",
-                        "Select the years:",
-                        min = 1,
-                        max = 50,
-                        value = 1),
-            sliderInput("scale",
-                        "Select the years:",
-                        min = 1,
-                        max = 20,
-                        value = 10)
+          sliderInput("range", "Range:",
+                      min = 1950, max = 2020,
+                      value = c(1950,2020))
         ),
         
         # Show a plot of the generated distribution
@@ -31,18 +39,23 @@ ui <- fluidPage(
     )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+# Define server logic required to draw a plot
+server <- function(input, output) { 
 
     output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x <- seq(1, 500, 1)
-        y <- dgamma(x, input$years, scale=input$scale)
-
-        # draw the histogram with the specified number of bins
-        plot(x, y, type="l", col = 'red', lwd=3,
-             xlab = 'x values',
-             main = 'plot of Gamma distrubution')
+      SQ_f <- SQ %>%
+        filter(year >= input$range[1] & year <= input$range[2])
+      
+      # Create a ggplot object
+      ggplot() +
+  
+        # Add the filtered sf object as a layer
+        geom_sf(data = SQ["state"]) +
+        geom_sf(data = SQ_f, aes(fill = occurrence)) +
+        
+        # Customize the plot as needed
+        theme_minimal() +
+        labs(title = "Occurrences Plot")
     })
 }
 
