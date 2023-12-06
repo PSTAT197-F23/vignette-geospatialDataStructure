2e3a59907c39e8ac67f2a78f02e49b442a66be48
Yibo <yiboliang02@gmail.com>
2023-12-05 17:07:57 -0800

    shiny app complete

--- GISshinyApp/app.R
+++ GISshinyApp/app.R
@@ -9,19 +9,21 @@ library(ggplot2)
 library(dplyr)
 
 us_species <- read_csv('./dat_species.csv')
-us_species <- us_species %>% filter(sciname == "Myocastor coypus" | sciname == "Hydrochoerus hydrochaeris")
+us_species <- us_species %>% filter(group == "Mammals"| group == "Reptiles" | group == "Amphibians")
 us_geo <- read_csv("./dat_spatial.csv")
 us_geo <- us_geo %>% 
   filter(name != "Hawaii" & name != "Alaska")
 
+names <- c(unique(us_species$sciname))
+
 colnames(us_geo)[1] <- "state"
 us_geo_merged <- merge(us_geo,us_species,by="state", all=T)
-names(us_geo_merged) 
 
 us_geo_merged <- us_geo_merged[is.na(us_geo_merged$geometry) == FALSE, ]
 
 # Create an sf object (SQ) from the WKT geometries
 SQ <- st_as_sf(us_geo_merged, wkt = "geometry")
+SQ1 <- st_as_sf(us_geo, wkt = "geometry")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -34,13 +36,15 @@ ui <- fluidPage(
         sidebarPanel(
           sliderInput("year", "Year:",
                       min = 1950, max = 2015,
-                      value = 1900)
-        ),
+                      value = 2000),
+          
+          radioButtons("name", "Species Name:",
+                       names)),
         
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+    # Show a plot of the generated distribution
+          mainPanel(
+            plotOutput("distPlot")
+          )
     )
 )
 
@@ -48,13 +52,17 @@ ui <- fluidPage(
 server <- function(input, output) { 
 
     output$distPlot <- renderPlot({
+      
       SQ_f <- SQ %>%
-        filter(year <= input$year)
+        filter(sciname == input$name) %>% 
+        filter(year <= input$year) %>% 
+        group_by(state) %>% 
+        summarise(occurrence = sum(occurrence))
       
       # Create a ggplot object
       ggplot() +
-  
         # Add the filtered sf object as a layer
+        geom_sf(data = SQ1["state"]) +
         geom_sf(data = SQ_f, aes(fill = occurrence)) +
         
         # Customize the plot as needed
